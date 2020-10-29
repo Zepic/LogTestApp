f1b99932c7ebfc4c5032b90fef71c9e52cce6cd0
Zepic <horto@hotmail.no>
2020-10-29 09:49:53 +0100

    Completed project

--- WebApplication2/Controllers/LogController.cs
+++ WebApplication2/Controllers/LogController.cs
@@ -14,18 +14,15 @@ namespace WebApplication2.Controllers
     [ApiController]
     public class LogController : ControllerBase
     {
-        //sql connection
         string _connStr =
-            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestLogDatabase;Integrated Security=True;"; // 
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestLogDatabase;Integrated Security=True;";
         string create = @"INSERT INTO LogDatabase(Date, DoneToday, DoingTomorrow)
                            VALUES (@Date, @DoneToday, @DoingTomorrow)";
 
         string readAll = @"SELECT Id, Date, DoneToday, DoingTomorrow
                             FROM LogDatabase";
 
-        string readOne = @"SELECT Id, Date, DoneToday, DoingTomorrow
-                            FROM LogDatabase
-                            WHERE Id = @Id";
+        string readLast = @"Select * from LogDatabase where Id = (select max(Id) from LogDatabase)";
 
         List<Log> _logList = new List<Log>();
         [HttpPost]
@@ -40,56 +37,19 @@ namespace WebApplication2.Controllers
         {
             var conn = new SqlConnection(_connStr);
             await conn.QueryAsync<Log>(readAll);
+
             return await Task.Run(() => _logList);
         }
 
         [HttpGet]
-        public async Task<Log> ReadLast()
+        public async Task<IEnumerable<Log>> ReadLast()
         {
             var conn = new SqlConnection(_connStr);
-            IEnumerable<Log> Logs = await conn.QueryAsync<Log>(readAll);
-            foreach (var log in Logs)
-            {
-                if (log != null)
-                {
-                    _logList.Add(new Log()
-                    {
-                        Id = log.Id,
-                        Date = log.Date,
-                        DoneToday = log.DoneToday,
-                        DoingTomorrow = log.DoingTomorrow
-                    });
-                }
-            }
-            return await Task.Run(() => 
-                Logs.FirstOrDefault(p => p.Id == _logList.Count)
-            );
+            var log = await conn.QueryAsync<Log>(readLast);
+            
+            return await Task.Run(() => log);
         }
 
-        /*
-            foreach (var log in Logs)
-            {
-                if (log != null)
-                {
-                    _logs.Add(new Log()
-                    {
-                        Id = log.Id, Date = log.Date, DoneToday = log.DoneToday, DoingTomorrow = log.DoingTomorrow
-                    });
-                }
-            }
-         */
-
-        // int rowsInserted1 = await conn.ExecuteAsync(create, new { Date = new DateTime(), DoneToday = "", DoingTomorrow = "" });
-        // IEnumerable<Log> Logs = await conn.QueryAsync<Log>(readAll);
-        // Log terje = await conn.QueryFirstOrDefaultAsync<Log>(readOne, new { Id = 0 });
-
-
-        /*
-         *int rowsInserted1 = await conn.ExecuteAsync(create, new { FirstName = "Terje", LastName = "Kolderup", BirthYear = 1975 });
-         * IEnumerable<Person> persons = await conn.QueryAsync<Person>(readMany);
-         *  Person terje = await conn.QueryFirstOrDefaultAsync<Person>(readOneByName, new { FirstName = "Terje" });
-         *
-         */
     }
 
 }
